47bb5c7328c041ef6b38ef5e94f71f79292056df
JulioMCruz-Contensive <julio@contensive.com>
2017-09-06 14:48:20 -0400

    Backend code

---
+++ source/aoArticleLibrary/Model/dbModels/LibraryCategories.vb
@@ -0,0 +1,3 @@
+﻿Public Class LibraryCategories
+
+End Class
---
+++ source/aoArticleLibrary/Model/dbModels/LibraryData.vb
@@ -0,0 +1,15 @@
+﻿Namespace Model.dbModels
+    '
+    <Serializable()>
+    Public Class LibraryData
+        Inherits BasicRecord
+        '
+        Public articleLibraryId As Integer = 0
+        Public articleLibraryCategoryID As Integer = 0
+        Public copy As String = ""
+        Public uploadFileName As String = ""
+        Public link As String
+        '
+    End Class
+    '
+End Namespace
